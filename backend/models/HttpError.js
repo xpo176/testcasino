c79139dae034482f3c2439f7d603be1d025c4376
Jonathan <87333013+xpo176@users.noreply.github.com>
2022-02-20 19:05:41 -0500

    Delete backend directory

--- backend/models/HttpError.js
+++
@@ -1,8 +0,0 @@
-class HttpError extends Error {
-    constructor(message, errorCode) {
-        super(message); // Add a message property for 
-        this.code = errorCode;
-        this.message = message;
-    }
-}
-module.exports = HttpError;
